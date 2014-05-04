98c81d8e204e179c2edfb68cfebe077026965cc5
rochefort <terasawan@gmail.com>
2014-05-05 02:12:13 +0900

    init

---
+++ lib/cocoapods/search.rb
@@ -0,0 +1,7 @@
+require "cocoapods/search/version"
+
+module Cocoapods
+  module Search
+    # Your code goes here...
+  end
+end
---
+++ lib/cocoapods/search/version.rb
@@ -0,0 +1,5 @@
+module Cocoapods
+  module Search
+    VERSION = "0.0.1"
+  end
+end
