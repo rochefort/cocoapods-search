33fdc0a26239ddc8ac1919b103cfd55ae1319570
rochefort <terasawan@gmail.com>
2014-05-10 19:41:23 +0900

    Remove requrie rubygems

--- lib/cocoapods/search.rb
+++ lib/cocoapods/search.rb
@@ -1,5 +1,3 @@
-require 'rubygems'
-
 module Cocoapods
   module Search
     class LibraryNotFound    < StandardError; end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'coveralls'
 Coveralls.wear!
 
