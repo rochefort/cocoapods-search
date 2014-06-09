b7e373b951057693fe1a5d833f8e002f1fa6951d
rochefort <terasawan@gmail.com>
2014-06-10 02:43:51 +0900

    Remove duplicate logic (refactoring)

--- lib/cocoapods/search/rendering.rb
+++ lib/cocoapods/search/rendering.rb
@@ -20,17 +20,23 @@ module Cocoapods::Search
         end
       end
 
-      def render_to_header
-        f=DEFAULT_RULED_LINE_SIZE.dup
+      def render_with
+        f = DEFAULT_RULED_LINE_SIZE.dup
         fmt = "%-#{f[0]}s %#{f[1]}s %#{f[2]}s %#{f[3]}s"
-        puts fmt % ['Name(Ver)', 'Score', 'Star', 'Fork']
-        puts fmt % ['-'*f[0], '-'*f[1], '-'*f[2], '-'*f[3]]
+        yield(f, fmt)
+      end
+
+      def render_to_header
+        render_with do |fields, fmt|
+          puts fmt % ['Name(Ver)', 'Score', 'Star', 'Fork']
+          puts fmt % fields.map{ |f| '-'*f }
+        end
       end
 
       def render_to_body(pods)
-        f=DEFAULT_RULED_LINE_SIZE.dup
-        fmt = "%-#{f[0]}s %#{f[1]}s %#{f[2]}s %#{f[3]}s"
-        pods.each { |pod| puts fmt % pod.to_a }
+        render_with do |fields, fmt|
+          pods.each { |pod| puts fmt % pod.to_a }
+        end
       end
   end
 end
