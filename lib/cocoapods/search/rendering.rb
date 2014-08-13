30d4b720d84f9f71b09cafcd5f207d7e94ef39a1
rochefort <terasawan@gmail.com>
2014-08-13 22:29:35 +0900

    Eliminate rubocop warnings: remove `set` method prefix

--- lib/cocoapods/search/rendering.rb
+++ lib/cocoapods/search/rendering.rb
@@ -7,37 +7,38 @@ module Cocoapods::Search
     end
 
     def render(pods)
-      set_ruled_line_size(pods)
+      ruled_line_size(pods)
       render_to_header
       render_to_body(pods)
     end
 
     private
-      def set_ruled_line_size(pods)
-        @ruled_line_size = DEFAULT_RULED_LINE_SIZE.dup
-        max_pod_name_size = pods.max_by { |pod| pod.name.size }.name.size
-        if max_pod_name_size > @ruled_line_size[0]
-          @ruled_line_size[0] = max_pod_name_size
-        end
-      end
 
-      def render_with
-        f = @ruled_line_size
-        fmt = "%-#{f[0]}s %#{f[1]}s %#{f[2]}s %#{f[3]}s"
-        yield(f, fmt)
+    def ruled_line_size(pods)
+      @ruled_line_size = DEFAULT_RULED_LINE_SIZE.dup
+      max_pod_name_size = pods.max_by { |pod| pod.name.size }.name.size
+      if max_pod_name_size > @ruled_line_size[0]
+        @ruled_line_size[0] = max_pod_name_size
       end
+    end
 
-      def render_to_header
-        render_with do |fields, fmt|
-          puts fmt % ['Name(Ver)', 'Score', 'Star', 'Fork']
-          puts fmt % fields.map{ |f| '-'*f }
-        end
-      end
+    def render_with
+      f = @ruled_line_size
+      fmt = "%-#{f[0]}s %#{f[1]}s %#{f[2]}s %#{f[3]}s"
+      yield(f, fmt)
+    end
 
-      def render_to_body(pods)
-        render_with do |fields, fmt|
-          pods.each { |pod| puts fmt % pod.to_a }
-        end
+    def render_to_header
+      render_with do |fields, fmt|
+        puts fmt % ['Name(Ver)', 'Score', 'Star', 'Fork']
+        puts fmt % fields.map{ |f| '-'*f }
       end
+    end
+
+    def render_to_body(pods)
+      render_with do |fields, fmt|
+        pods.each { |pod| puts fmt % pod.to_a }
+      end
+    end
   end
 end
